58be3541ec2b1c2e1d6900f58f752abb99c75d7f
Steve Cox <scox@modelmetrics.com>
2013-08-31 15:56:07 -0400

    #5031, #5032

---
+++ src/ModelComparer.java
@@ -0,0 +1,7 @@
+import java.util.Comparator;
+
+public class ModelComparer implements Comparator<Model> {
+  public int compare(Model o1, Model o2) {
+    return o1.getName().compareToIgnoreCase(o2.getName());
+  }
+}
